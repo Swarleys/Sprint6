2e7cce5631a31c601e2fc08c5e0d416ba997344b
Esteban Ramos Coronado <esteban@Mac-mini-de-Esteban.local>
2022-03-01 14:54:29 +0100

    Exercici 1

---
+++ Vue/src/components/Home.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Scene from "./Scene.vue";
+</script>
+
+<template>
+    <Scene
+        msg="El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"
+    />
+</template>
+
+<style scoped>
+</style>
