98937e8f4b2e5df06e7066f6c31beb7ae0f338c2
Esteban Ramos Coronado <esteban@Mac-mini-de-Esteban.local>
2022-03-02 00:22:20 +0100

    Exercici 5

--- Vue/src/components/Home.vue
+++ Vue/src/components/Home.vue
@@ -4,13 +4,12 @@ import messages from "../assets/data.json";
 import Botons from "./Botons.vue";
 import { ref } from 'vue';
 const currentSentence = ref(0);
+const started = ref(false);
 
 const increase = (): number => {
     if (currentSentence.value < 3) {
         currentSentence.value++;
     }
-    console.log(currentSentence.value);
-    
     return currentSentence.value;
 }
 const decrease = (): number => {
@@ -23,8 +22,15 @@ const decrease = (): number => {
 </script>
 
 <template>
-    <Botons @nextScene="increase()" @prevScene="decrease()" />
-    <Scene :data="messages" :scene="currentSentence" />
+    <div v-if="!started" class="flex flex-col justify-center items-center gap-20 mt-20">
+        <h1 class="text-6xl">Sprint6 Vue</h1>
+        <p class="max-w-6xl text-center text-3xl">Aquest es el primer projecte que fem amb Vue al Sprint 6, es una historia interactiva que funciona fent click en un parell de butons que serveixen per avançar o retrocedir l'historia. Fes click en el següent boto quant estiguis preparat</p>
+        <button type="button" @click="() => started = true" class="text-3xl p-4 bg-slate-300 rounded-lg border-2 border-slate-700">Comencem!</button>
+    </div>
+    <div v-if="started">
+        <Botons @nextScene="increase()" @prevScene="decrease()" />
+        <Scene :data="messages" :scene="currentSentence" />
+    </div>
 </template>
 
 <style scoped>
