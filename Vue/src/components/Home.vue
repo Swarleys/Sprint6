783d1e4b73aff89aa3353e7e76f5b54f9d0591a2
Esteban Ramos Coronado <esteban@Mac-mini-de-Esteban.local>
2022-03-01 23:56:04 +0100

    Exercici 4

--- Vue/src/components/Home.vue
+++ Vue/src/components/Home.vue
@@ -1,10 +1,30 @@
 <script setup lang="ts">
 import Scene from "./Scene.vue";
-import  messages  from "../assets/data.json";
+import messages from "../assets/data.json";
+import Botons from "./Botons.vue";
+import { ref } from 'vue';
+const currentSentence = ref(0);
+
+const increase = (): number => {
+    if (currentSentence.value < 3) {
+        currentSentence.value++;
+    }
+    console.log(currentSentence.value);
+    
+    return currentSentence.value;
+}
+const decrease = (): number => {
+    if (currentSentence.value > 0) {
+        currentSentence.value--;
+    }
+    return currentSentence.value;
+}
+
 </script>
 
 <template>
-    <Scene :data="messages" />
+    <Botons @nextScene="increase()" @prevScene="decrease()" />
+    <Scene :data="messages" :scene="currentSentence" />
 </template>
 
 <style scoped>
--- Vue/src/components/Scene.vue
+++ Vue/src/components/Scene.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
 import { Messages } from "../Interfaces/interfaces"
-defineProps<{ data: Messages }>()
+defineProps<{ data: Messages, scene: number }>()
 </script>
 
 <template>
   <ul class="flex flex-col justify-center gap-6 mt-6">
-    <li v-for="message, index in data.messages" :key="index" class="text-center border-2 py-4 mx-8 rounded-[50px] border-black ">{{ message }}</li>
+    <li
+      v-for="message, index in data.messages"
+      :key="index"
+      class="text-center border-2 py-4 mx-8 rounded-[50px] border-black"
+      :class="{ active: index === scene }"
+    >{{ message }}</li>
   </ul>
 </template>
 
 <style scoped>
+.active {
+  @apply bg-red-200;
+}
 </style>
